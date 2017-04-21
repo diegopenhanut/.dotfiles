11a09d1de4a994834a5570fa863ed53be7ac3446
diegopenhanut <diegopenhanut@gmail.com>
2017-04-20 22:56:04 -0300

    add r packages dependencies

--- setup.r.debian.sh
+++ setup.r.debian.sh
@@ -6,7 +6,7 @@ sudo echo "deb http://vps.fmvz.usp.br/CRAN/bin/linux/debian  jessie-cran3/" > /e
 sudo apt-get update
 sudo apt-get install r-base r-base-dev
 sudo apt-get install pandoc
-
+sudo apt-get install r-cran-curl libssl-dev libxml2-dev
 # install packages
 R -e "install.packages('tidyverse', repos='http://cran.rstudio.com/')"
-R -e "install.packages('devtools', repos='http://cran.rstudio.com/')"
+R -e "install.packages(c('httr', 'rvest', 'xml2', 'devtools'), repos='http://cran.rstudio.com/')"
