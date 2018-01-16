155631681386ccf0970714efb62849ac412167fc
diegopenhanut <diegopenhanut@gmail.com>
2018-01-16 12:54:02 -0300

    add pt-br

--- setup.latex.sh
+++ setup.latex.sh
@@ -4,6 +4,7 @@
 sudo apt-get install --no-install-recommends texlive-latex-extra
 sudo apt-get install texlive-fonts-recommended texlive-fonts-extra
 sudo apt-get install lmodern
+sudo apt-get install texlive-lang-portuguese
 
 # try those if it did not work
 # sudo apt-get install lmodern
