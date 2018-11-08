df592f9b759843815871cae0a2c4b887a510a581
diegopenhanut <diegopenhanut@gmail.com>
2018-11-08 14:43:37 -0300

    add \| to mapping

--- switch.keyboard.sh
+++ switch.keyboard.sh
@@ -1,3 +1,5 @@
+#!
+
 # Help para keyboard
 # F1-F3 estão funcionando, F4-F11 precisam de 
 # change apple keyboard fn status
@@ -6,4 +8,7 @@
 # sudo bash -c "echo 1 > /sys/module/hid_apple/parameters/swap_opt_cmd"
 # turn home into esc
 xmodmap -e "keycode 180 = Escape"
-xmodmap -e "keycode 134 = backslash bar backslash bar masculine dead_breve masculine"
+xmodmap -e "keycode 134 = slash question slash question degree questiondown degree questiondown slash question degree questiondown slash question degree questiondown"
+xmodmap -e "keycode 26 = e E e E backslash EuroSign EuroSign EuroSign e E EuroSign EuroSign e E EuroSign EuroSign"
+xmodmap -e "keycode 27 = r R r R bar registered registered registered r R registered registered r R registered registered"
+
