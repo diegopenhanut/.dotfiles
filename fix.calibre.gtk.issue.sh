16880968ba5db265ad736935eb496257cd5ebd06
diegopenhanut <diegopenhanut@gmail.com>
2019-01-11 08:53:50 -0300

    organize files

--- fix.calibre.gtk.issue.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#http://www.mobileread.com/forums/archive/index.php/t-257394.html
-calibre-debug -c "from calibre.gui2 import gprefs; gprefs['ui_style'] = 'calibre'"
-
--- libinput_gestures_touchpad.sh
+++
@@ -1,7 +0,0 @@
-sudo gpasswd -a $USER input
-
-sudo apt-get install xdotool wmctrl libinput-tools
-
-git clone https://github.com/bulletmark/libinput-gestures.git
-cd libinput-gestures
-sudo make install && echo "please, reeboot and run libinput-gestures-setup autostart"
