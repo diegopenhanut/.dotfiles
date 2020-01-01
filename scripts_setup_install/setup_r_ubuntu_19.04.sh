6eab5aa5690db55678ad8c987eec20655391b0e4
diegopenhanut <diegopenhanut@gmail.com>
2020-01-01 20:58:52 -0300

    update after moving to new computer

--- scripts_setup_install/setup_r_ubuntu_19.04.sh
+++ scripts_setup_install/setup_r_ubuntu_19.04.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
-
 #gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E084DAB9
 #gpg -a --export E084DAB9 | sudo apt-key add -
+#gpg --keyserver keyserver.ubuntu.com --recv-key E298A3A825C0D65DFD57CBB651716619E084DAB9
+#gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | sudo apt-key add -
+
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu disco-cran35/'
 
 sudo apt-get update
 sudo apt-get install r-base pandoc libcurl4-openssl-dev libxml2-dev gdebi libxml2-dev libssl-dev libcurl4-openssl-dev libopenblas-dev
